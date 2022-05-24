818a84ccdd6fef96bad6229fa8416f4ba57505e8
Marzieh <marzy1997@gmail.com>
2022-05-25 01:18:21 +0430

    add solutions

--- solution3.py
+++ solution3.py
@@ -1,5 +1,15 @@
-a=600851475143/23
 
-print(a)
-
+m=0
+z=[600851475143]
+for i in range(2,z[-1]):
     
+    if z[-1]%i==0:
+        
+        x=z[-1]/i
+        z.append(x)
+        
+    elif z[-1]==1:
+         break
+       
+        
+print(z[-2])
