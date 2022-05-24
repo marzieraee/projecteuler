c44602fe9b8524022acc9e58b0373a8d76ebcdf3
Marzieh <marzy1997@gmail.com>
2022-05-24 15:04:52 +0430

    change solution1

--- solution1.py
+++ solution1.py
@@ -1,16 +1,20 @@
 max=1000
 a=3
 b=5
-list1=[]
+list1=0
 
 while b<max:
-    list1.append(b)
+    list1+=b
     b+=5
 
-while a<max:
-    list1.append(a)
+while a<max :
+    list1+=a
     a+=3
+for i in range(1000) :
+    if i%3==0 and i%5==0 :
+        list1 -=i
 
-print(sum(list1))
+
+print(list1)
 
 
